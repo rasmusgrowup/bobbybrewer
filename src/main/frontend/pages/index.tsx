28b9e37a0fc356e21ca0934c9f30563bc7cef8e5
SjuChefen <madsbraae94@gmail.com>
2023-11-14 14:45:16 +0100

    Real-time update and progress bars

--- src/main/frontend/pages/index.tsx
+++ src/main/frontend/pages/index.tsx
@@ -1,49 +1,108 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {Button, Grid, Container, FormControl, FormControlLabel, Input, InputLabel, Radio, RadioGroup, Slider} from "@mui/material";
-import { Unstable_NumberInput as NumberInput } from '@mui/base/Unstable_NumberInput';
-import {useEffect, useState} from "react";
-import { useSlider } from '@mui/base/useSlider';
+import {
+    Button,
+    Grid,
+    Container,
+    FormControl,
+    FormControlLabel,
+    LinearProgressProps,
+    Input,
+    InputLabel,
+    Radio,
+    RadioGroup,
+    Slider, LinearProgress, Box, Typography
+} from "@mui/material";
+import {Unstable_NumberInput as NumberInput} from '@mui/base/Unstable_NumberInput';
+import React, {useEffect, useState} from "react";
+import {useSlider} from '@mui/base/useSlider';
+import {Simulate} from "react-dom/test-utils";
+import progress = Simulate.progress;
 
+function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ width: '100px', mr: 1 }}>
+                <LinearProgress variant="determinate" {...props} />
+            </Box>
+            <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="text.secondary">{`${Math.round(
+                    props.value,
+                )}%`}</Typography>
+            </Box>
+        </Box>
+    );
+}
 const Home: NextPage = () => {
-    const [data, setData] = useState<string | null>(null);
+    const [data, setData] = useState({
+        stateCurrent:null,
+        totalProduced:null,
+        goodProducts:null,
+        badProducts:null,
+        maintenanceCounter:0,
+        inventoryYeast:0,
+        inventoryWheat:0,
+        inventoryMalt:0,
+        inventoryHops:0,
+        inventoryBarley:0,
+        humidity:null,
+        temperature:null,
+        vibration:null
+    });
     const [isLoading, setLoading] = useState<boolean>(true);
     const [isProducing, setProducing] = useState<boolean>(false);
     const [amount, setAmount] = useState(100);
     const [speed, setSpeed] = useState(30);
     const [beerType, setBeerType] = useState(0);
+    const [progressYeast, setProgressYeast] = useState(0);
+    const [progressWheat, setProgressWheat] = useState(0);
+    const [progressMalt, setProgressMalt] = useState(0);
+    const [progressHops, setProgressHops] = useState(0);
+    const [progressBarley, setProgressBarley] = useState(0);
+    const [progressMaintenance, setProgressMaintenance] = useState(0);
+    //const {stateCurrent} = data;
+
+    useEffect(()=> {
+        setProgressYeast(data.inventoryYeast/35000*100);
+        setProgressWheat(data.inventoryWheat/35000*100);
+        setProgressMalt(data.inventoryMalt/35000*100);
+        setProgressHops(data.inventoryHops/35000*100);
+        setProgressBarley(data.inventoryBarley/35000*100);
+        setProgressMaintenance(data.maintenanceCounter/65535*100);
+    },[data])
 
     useEffect(() => {
         setLoading(true);
-        const fetchData = () => { fetch('/api/read-current-state')
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                return res.json();
-            })
-            .then(data => {
-                setData(data.data); // Assuming your JSON structure includes a "payload" property
-                setLoading(false);
-            })
-            .catch(error => {
-                setData(error.toString());
-                console.error('An error occurred:', error);
-            });
-    };
+        const fetchData = () => {
+            fetch('/api/read-current-state')
+                .then(res => {
+                    if (!res.ok) {
+                        throw new Error("Network response was not ok");
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data); // Assuming your JSON structure includes a "payload" property
+                    setLoading(false);
+                })
+                .catch(error => {
+                    setData(error.toString());
+                    console.error('An error occurred:', error);
+                });
+        };
         // Call the function once immediately, then set the interval
         fetchData();
-        const intervalId = setInterval(fetchData, 500); // 1000ms = 1 second
+        const intervalId = setInterval(fetchData, 100); // 1000ms = 1 second
 
         // Cleanup function to clear the interval when the component unmounts
         return () => clearInterval(intervalId);
     }, []);
 
 
-    console.log("amount: " + amount);
-    console.log("slider value: " + speed);
-    console.log("Beer type: " + beerType);
+    console.log(data);
+    //console.log("slider value: " + speed);
+    //console.log("Beer type: " + beerType);
 
     const handleStartProduction = async () => {
         // Here, you can make a fetch request to send messages and multiple commands to the OPC server
@@ -118,17 +177,37 @@ const Home: NextPage = () => {
 
             <main className={styles.main}>
                 <h1>Waesome webpage</h1>
-                 <div>Status of the machine: {data}</div>
+                <div>Status of the machine: {data.stateCurrent}</div>
+                <div>totalProduced: {data.totalProduced}</div>
+                <div>goodProducts: {data.goodProducts}</div>
+                <div>badProducts: {data.badProducts}</div>
+                <div>maintenanceCounter: {data.maintenanceCounter}</div>
+                <div>inventoryYeast: {data.inventoryYeast}</div>
+                <div>inventoryWheat: {data.inventoryWheat}</div>
+                <div>inventoryMalt: {data.inventoryMalt}</div>
+                <div>inventoryHops: {data.inventoryHops}</div>
+                <div>inventoryBarley: {data.inventoryBarley}</div>
+                <div>humidity: {data.humidity}</div>
+                <div>temperature: {data.temperature}</div>
+                <div>vibration: {data.vibration}</div>
+                <LinearProgressWithLabel value={progressYeast} />
+                <LinearProgressWithLabel value={progressWheat} />
+                <LinearProgressWithLabel value={progressMalt} />
+                <LinearProgressWithLabel value={progressHops} />
+                <LinearProgressWithLabel value={progressBarley} />
+                <LinearProgressWithLabel value={progressMaintenance} />
                 <div className={styles.form}>
                     <div className={styles.column}>
                         <label>Choose a beer type</label>
-                        <RadioGroup onChange={(event, val:any) => setBeerType(val)} value={beerType} aria-labelledby="demo-radio-buttons-group-label" defaultValue="pilsner" name="radio-buttons-group">
-                            <FormControlLabel value="0" control={<Radio />} label="Pilsner"/>
-                            <FormControlLabel value="1" control={<Radio />} label="Wheat" />
-                            <FormControlLabel value="2" control={<Radio />} label="IPA" />
-                            <FormControlLabel value="3" control={<Radio />} label="Stout" />
-                            <FormControlLabel value="4" control={<Radio />} label="Ale" />
-                            <FormControlLabel value="5" control={<Radio />} label="Alcohol-free" />
+                        <RadioGroup onChange={(event, val: any) => setBeerType(val)} value={beerType}
+                                    aria-labelledby="demo-radio-buttons-group-label" defaultValue="pilsner"
+                                    name="radio-buttons-group">
+                            <FormControlLabel value="0" control={<Radio/>} label="Pilsner"/>
+                            <FormControlLabel value="1" control={<Radio/>} label="Wheat"/>
+                            <FormControlLabel value="2" control={<Radio/>} label="IPA"/>
+                            <FormControlLabel value="3" control={<Radio/>} label="Stout"/>
+                            <FormControlLabel value="4" control={<Radio/>} label="Ale"/>
+                            <FormControlLabel value="5" control={<Radio/>} label="Alcohol-free"/>
                         </RadioGroup>
                     </div>
                     <div className={styles.column}>
@@ -137,7 +216,7 @@ const Home: NextPage = () => {
                             aria-label="Amount of beer"
                             placeholder="Choose amount of beer"
                             value={amount}
-                            onChange={(event, val:any) => setAmount(val)}
+                            onChange={(event, val: any) => setAmount(val)}
                             step={100}
                         />
                         <label className={styles.speedLabel}>Choose production speed: {speed}</label>
@@ -150,8 +229,10 @@ const Home: NextPage = () => {
                             max={100}
                             onChange={(_, newValue: any) => setSpeed(newValue)}
                         />
-                        <Button className={styles.formButton} type="submit" variant={"contained"} onClick={() => handleStartProduction()} disabled={isProducing}>Start</Button>
-                        <Button className={styles.formButton} type="submit" variant={"outlined"} onClick={() => handleStopProduction()} disabled={!isProducing}>Stop</Button>
+                        <Button className={styles.formButton} type="submit" variant={"contained"}
+                                onClick={() => handleStartProduction()} disabled={data.stateCurrent == 6}>Start</Button>
+                        <Button className={styles.formButton} type="submit" variant={"outlined"}
+                                onClick={() => handleStopProduction()} disabled={data.stateCurrent != 6}>Stop</Button>
                     </div>
                 </div>
                 <div className={styles.buttons}>
--- src/main/java/org/app/springnext/demo/OPCController.java
+++ src/main/java/org/app/springnext/demo/OPCController.java
@@ -16,8 +16,38 @@ public class OPCController implements IOPCController {
         Map<String, String> responseData = new HashMap<>();
         try {
             OpcUaClient opcClient = OpcUaClientSingleton.getInstance();
-            NodeId readStatus = new NodeId(6, "::Program:Cube.Admin.ProdProcessedCount");
-            responseData.put("data", OpcUaUtility.readValue(opcClient, readStatus));
+            //Current state:
+            NodeId stateCurrent = new NodeId(6, "::Program:Cube.Status.StateCurrent");
+            responseData.put("stateCurrent", OpcUaUtility.readValue(opcClient, stateCurrent));
+            //Product:
+            NodeId totalProduced = new NodeId(6, "::Program:product.produced");
+            NodeId goodProducts = new NodeId(6, "::Program:product.good");
+            NodeId badProducts = new NodeId(6, "::Program:product.bad");
+            responseData.put("totalProduced", OpcUaUtility.readValue(opcClient, totalProduced));
+            responseData.put("goodProducts", OpcUaUtility.readValue(opcClient, goodProducts));
+            responseData.put("badProducts", OpcUaUtility.readValue(opcClient, badProducts));
+            //Maintenance:
+            NodeId maintenanceCounter = new NodeId(6, "::Program:Maintenance.Counter");
+            responseData.put("maintenanceCounter", OpcUaUtility.readValue(opcClient, maintenanceCounter));
+            //Inventory:
+            NodeId inventoryYeast = new NodeId(6, "::Program:Inventory.Yeast");
+            NodeId inventoryWheat = new NodeId(6, "::Program:Inventory.Wheat");
+            NodeId inventoryMalt = new NodeId(6, "::Program:Inventory.Malt");
+            NodeId inventoryHops = new NodeId(6, "::Program:Inventory.Hops");
+            NodeId inventoryBarley = new NodeId(6, "::Program:Inventory.Barley");
+            responseData.put("inventoryYeast", OpcUaUtility.readValue(opcClient, inventoryYeast));
+            responseData.put("inventoryWheat", OpcUaUtility.readValue(opcClient, inventoryWheat));
+            responseData.put("inventoryMalt", OpcUaUtility.readValue(opcClient, inventoryMalt));
+            responseData.put("inventoryHops", OpcUaUtility.readValue(opcClient, inventoryHops));
+            responseData.put("inventoryBarley", OpcUaUtility.readValue(opcClient, inventoryBarley));
+            //Sensors:
+            NodeId humidity = new NodeId(6, "::Program:Cube.Status.Parameter[2].Value");
+            NodeId temperature = new NodeId(6, "::Program:Cube.Status.Parameter[3].Value");
+            NodeId vibration = new NodeId(6, "::Program:Cube.Status.Parameter[4].Value");
+            responseData.put("humidity", OpcUaUtility.readValue(opcClient, humidity));
+            responseData.put("temperature", OpcUaUtility.readValue(opcClient, temperature));
+            responseData.put("vibration", OpcUaUtility.readValue(opcClient, vibration));
+
             return responseData;
         } catch (Exception e) {
             responseData.put("error", "Failed to read data.");
@@ -55,7 +85,7 @@ public class OPCController implements IOPCController {
         commandController.resetCommand();
         while (!is_reset){
             is_reset = OpcUaUtility.readValue(OpcUaClientSingleton.getInstance(), new NodeId(6, "::Program:Cube.Status.StateCurrent")).equals("4");
-            System.out.println(OpcUaUtility.readValue(OpcUaClientSingleton.getInstance(), new NodeId(6, "::Program:Cube.Status.StateCurrent")));
+            //System.out.println(OpcUaUtility.readValue(OpcUaClientSingleton.getInstance(), new NodeId(6, "::Program:Cube.Status.StateCurrent")));
         }
         commandController.setBeerType(requestBody);
         commandController.setAmount(requestBody);
@@ -64,6 +94,7 @@ public class OPCController implements IOPCController {
     }
 
     @Override
+    @PostMapping("/stop_production")
     public void cancelProduction() {
         CommandController commandController = new CommandController();
         commandController.cancelProduction();

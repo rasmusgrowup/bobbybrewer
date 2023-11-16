fcb9db6cdb61d9216fceaa2d10976e963c7185ea
Rasmus Andersen <rasmus@growupstudio.dk>
2023-11-16 12:08:26 +0100

    even more StatusContainer

--- src/main/frontend/components/StatusContainer.js
+++ src/main/frontend/components/StatusContainer.js
@@ -1,12 +1,26 @@
 import styles from '../styles/Status.module.css'
 
-export default function StatusContainer({data}) {
-    console.log(data)
+export default function StatusContainer(data, status) {
+    let content;
+
+    switch (data) {
+        case '0':
+            content = <p>Option 1 is selected</p>;
+            break;
+        case '1':
+            content = <p>Option 2 is selected</p>;
+            break;
+        case '2':
+            content = <p>Option 3 is selected</p>;
+            break;
+        default:
+            content = <p></p>;
+    }
 
     return (
         <div className={styles.container}>
             <div>
-                <div className={styles.status}>System status</div>
+                <div className={styles.status}>System status: {content}</div>
                     <div className={styles.response}>
                         {!data ? <div>Offline</div> : <div>Online</div>}
                         {!data ? <div className={styles.redDot}/> : <div className={styles.greenDot}/>}
